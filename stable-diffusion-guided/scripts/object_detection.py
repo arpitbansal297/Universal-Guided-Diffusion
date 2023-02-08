5eec418fa9b4dfaff4fc720b9a7873b400c880aa
Hong-Min Chu <hmchu@nexuscml00.umiacs.umd.edu>
2023-02-07 21:28:41 -0500

    Further clean up of OD for stable diffusion

--- stable-diffusion-guided/scripts/object_detection.py
+++ stable-diffusion-guided/scripts/object_detection.py
@@ -1,6 +1,8 @@
 import sys
 import os
 from pathlib import Path
+import os
+import errno
 
 sys.path.append(str(Path(os.path.abspath(__file__)).parents[1]))
 
@@ -14,7 +16,6 @@ import numpy as np
 from omegaconf import OmegaConf
 from PIL import Image
 from tqdm import tqdm, trange
-#from imwatermark import WatermarkEncoder
 from itertools import islice
 from einops import rearrange
 from torchvision.utils import make_grid
@@ -26,11 +27,9 @@ from contextlib import contextmanager, nullcontext
 
 from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
-from ldm.models.diffusion.plms import PLMSSampler
 
 from ldm.models.diffusion.ddim_with_grad import DDIMSamplerWithGrad
 
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
 
 from torchvision import transforms, utils
@@ -50,34 +49,10 @@ import clip
 import os
 import inspect
 import torchvision.transforms.functional as TF
-from facenet_pytorch import MTCNN, InceptionResnetV1
-from torchvision.datasets import ImageFolder
 
 import pickle
 
 
-
-# load safety model
-safety_model_id = "CompVis/stable-diffusion-safety-checker"
-safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
-safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
-
-
-def chunk(it, size):
-    it = iter(it)
-    return iter(lambda: tuple(islice(it, size)), ())
-
-
-def numpy_to_pil(images):
-    """
-    Convert a numpy image or a batch of images to a PIL image.
-    """
-    if images.ndim == 3:
-        images = images[None, ...]
-    images = (images * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
-
-    return pil_images
 
 
 def load_model_from_config(config, ckpt, verbose=False):
@@ -99,59 +74,6 @@ def load_model_from_config(config, ckpt, verbose=False):
     model.eval()
     return model
 
-
-def put_watermark(img, wm_encoder=None):
-    if wm_encoder is not None:
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img = wm_encoder.encode(img, 'dwtDct')
-        img = Image.fromarray(img[:, :, ::-1])
-    return img
-
-
-def load_replacement(x):
-    try:
-        hwc = x.shape
-        y = Image.open("assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
-        y = (np.array(y)/255.0).astype(x.dtype)
-        assert y.shape == x.shape
-        return y
-    except Exception:
-        return x
-
-
-def check_safety(x_image):
-    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
-    assert x_checked_image.shape[0] == len(has_nsfw_concept)
-    for i in range(len(has_nsfw_concept)):
-        if has_nsfw_concept[i]:
-            x_checked_image[i] = load_replacement(x_checked_image[i])
-    return x_checked_image, has_nsfw_concept
-
-class Dataset(data.Dataset):
-    def __init__(self, folder, image_size, data_aug=False, exts=['jpg', 'jpeg', 'png']):
-        super().__init__()
-        self.folder = folder
-        self.image_size = image_size
-        self.paths = [p for ext in exts for p in Path(f'{folder}').glob(f'**/*.{ext}')]
-        random.shuffle(self.paths)
-
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Lambda(lambda t: (t * 2) - 1)
-        ])
-
-    def __len__(self):
-        return len(self.paths)
-
-    def __getitem__(self, index):
-        path = self.paths[index]
-        img = Image.open(path)
-        img = img.convert('RGB')
-        img = img.resize((self.image_size, self.image_size), resample=PIL.Image.LANCZOS)
-
-        return self.transform(img)
-
 def return_cv2(img, path):
     black = [255, 255, 255]
     img = (img + 1) * 0.5
@@ -160,13 +82,6 @@ def return_cv2(img, path):
     img = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=black)
     return img
 
-def cycle(dl):
-    while True:
-        for data in dl:
-            yield data
-
-import os
-import errno
 def create_folder(path):
     try:
         os.makedirs(path, exist_ok=True)
@@ -216,19 +131,7 @@ class ObjectDetection(nn.Module):
 
 
 
-def cycle_cat(dl):
-    while True:
-        for data in dl:
-            yield data[0]
-
-def l1_loss(input, target):
-    l = torch.abs(input - target).mean(dim=[1])
-    return l
-
 def get_optimation_details(args):
-    mtcnn_face = not args.center_face
-    print('mtcnn_face')
-    print(mtcnn_face)
 
     guidance_func = ObjectDetection().cuda()
     operation = OptimizerDetails()
@@ -238,7 +141,7 @@ def get_optimation_details(args):
 
     operation.optimizer = 'Adam'
     operation.lr = args.optim_lr
-    operation.loss_func = l1_loss
+    operation.loss_func = None
 
     operation.max_iters = args.optim_max_iters
     operation.loss_cutoff = args.optim_loss_cutoff
@@ -260,61 +163,16 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--prompt",
-        type=str,
-        nargs="?",
-        default="",
-        help="the prompt to render"
-    )
-    parser.add_argument(
-        "--outdir",
-        type=str,
-        nargs="?",
-        help="dir to write results to",
-        default="outputs/txt2img-samples"
-    )
-    parser.add_argument(
-        "--skip_grid",
-        action='store_true',
-        help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
-    )
-    parser.add_argument(
-        "--skip_save",
-        action='store_true',
-        help="do not save individual samples. For speed measurements.",
-    )
-    parser.add_argument(
         "--ddim_steps",
         type=int,
         default=50,
         help="number of ddim sampling steps",
     )
     parser.add_argument(
-        "--plms",
-        action='store_true',
-        help="use plms sampling",
-    )
-    parser.add_argument(
-        "--laion400m",
-        action='store_true',
-        help="uses the LAION400M model",
-    )
-    parser.add_argument(
-        "--fixed_code",
-        action='store_true',
-        help="if enabled, uses the same starting code across samples ",
-    )
-    parser.add_argument(
         "--ddim_eta",
         type=float,
         default=0.0,
         help="ddim eta (eta=0.0 corresponds to deterministic sampling",
-    )
-    parser.add_argument(
-        "--n_iter",
-        type=int,
-        default=2,
-        help="sample this often",
     )
     parser.add_argument(
         "--H",
@@ -343,25 +201,14 @@ def main():
     parser.add_argument(
         "--n_samples",
         type=int,
-        default=3,
+        default=1,
         help="how many samples to produce for each given prompt. A.k.a. batch size",
-    )
-    parser.add_argument(
-        "--n_rows",
-        type=int,
-        default=0,
-        help="rows in the grid (default: n_samples)",
     )
     parser.add_argument(
         "--scale",
         type=float,
         default=7.5,
         help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
-    )
-    parser.add_argument(
-        "--from-file",
-        type=str,
-        help="if specified, load prompts from this file",
     )
     parser.add_argument(
         "--config",
@@ -381,13 +228,6 @@ def main():
         default=42,
         help="the seed (for reproducible sampling)",
     )
-    parser.add_argument(
-        "--precision",
-        type=str,
-        help="evaluate at this precision",
-        choices=["full", "autocast"],
-        default="autocast"
-    )
 
     parser.add_argument("--optim_lr", default=1e-2, type=float)
     parser.add_argument('--optim_max_iters', type=int, default=1)
@@ -405,25 +245,13 @@ def main():
     parser.add_argument('--optim_folder', default='./temp/')
     parser.add_argument("--optim_num_steps", nargs="+", default=[1], type=int)
     parser.add_argument("--optim_mask_fraction", default=0.5, type=float)
-    parser.add_argument("--text", default=None)
     parser.add_argument('--text_type', type=int, default=1)
-    parser.add_argument("--batches", default=0, type=int)
-
-    parser.add_argument('--fr_crop', action='store_true')
-    parser.add_argument('--center_face', action='store_true')
-    parser.add_argument('--big_text', action='store_true')
-
+    parser.add_argument("--trials", type=int, default=10)
 
     opt = parser.parse_args()
 
     results_folder = opt.optim_folder
     create_folder(results_folder)
-
-    if opt.laion400m:
-        print("Falling back to LAION 400M model...")
-        opt.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
-        opt.ckpt = "models/ldm/text2img-large/model.ckpt"
-        opt.outdir = "outputs/txt2img-samples-laion400m"
 
     seed_everything(opt.seed)
 
@@ -438,23 +266,8 @@ def main():
 
     sampler = DDIMSamplerWithGrad(model)
 
-    os.makedirs(opt.outdir, exist_ok=True)
-    outpath = opt.outdir
-
-    print("Creating invisible watermark encoder (see https://github.com/ShieldMnt/invisible-watermark)...")
-    #wm = "StableDiffusionV1"
-    #wm_encoder = WatermarkEncoder()
-    #wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
-
     batch_size = opt.n_samples
-    n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
-
-    sample_path = os.path.join(outpath, "samples")
-    os.makedirs(sample_path, exist_ok=True)
-    base_count = len(os.listdir(sample_path))
-    grid_count = len(os.listdir(outpath)) - 1
-
-
+    assert batch_size == 1
 
     operation = get_optimation_details(opt)
 
@@ -464,93 +277,17 @@ def main():
         transforms.ToTensor(),
         transforms.Lambda(lambda t: (t * 2) - 1)
     ])
-    ds = ImageFolder(root='/cmlscratch/hmchu/datasets/celeba_hq_256/', transform=transform)
-    dl = cycle_cat(data.DataLoader(ds, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=16,
-                                   drop_last=True))
 
     torch.set_grad_enabled(False)
 
     if opt.text_type == 1:
-        prompt = "a photograph of two cats near a river"
-        obj_det_cats = ["cat", "cat"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [170, 170]
-    elif opt.text_type == 2:
-        prompt = "a photograph of a cat and a dog near a river"
-        obj_det_cats = ["cat", "dog"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [170, 170]
-    elif opt.text_type == 3:
-        prompt = "an oil painting of two cats near a river"
-        obj_det_cats = ["cat", "cat"]
-        test_anchor_locs = [(128.0, 128.0), (384.0, 384.0)]
-        sizes = [200, 140]
-    elif opt.text_type == 4:
-        prompt = "an oil painting of a cat and a dog near a river"
-        obj_det_cats = ["cat", "dog"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [200, 140]
-    elif opt.text_type == 5:
-        prompt = "an oil painting of a cat and a dog near a river"
-        obj_det_cats = ["cat", "dog"]
-        test_anchor_locs = [(128.0, 128.0), (384.0, 384.0)]
-        sizes = [170, 170]
-    elif opt.text_type == 6:
         prompt = "a headshot of a woman with a dog"
         obj_det_cats = ["dog", "person"]
         test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
         sizes = [180, [200,400]]
-    elif opt.text_type == 7:
-        prompt = "a headshot of a man with a dog"
-        obj_det_cats = ["person", "dog"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [[200, 400], 180]
-    elif opt.text_type == 8:
-        prompt = "two chairs and a person in a room"
-        obj_det_cats = ["chair", "person", "chair"]
-        test_anchor_locs = [(100.0, 400.0), (256.0, 256.0), (400.0, 256.0)]
-        sizes = [140, [150, 400], [200, 200]]
-    elif opt.text_type == 9:
-        prompt = "an oil painting of a headshot of a woman with a dog"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-    elif opt.text_type == 10:
-        prompt = "a headshot of a woman with a dog in space"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-    elif opt.text_type == 11:
-        prompt = "a headshot of a woman with a dog on beach"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-    elif opt.text_type == 12:
-        prompt = "a headshot of a woman with a dog in winter"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-    elif opt.text_type == 13:
-        prompt = "a headshot of a woman with a dog on new york street"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-    elif opt.text_type == 14:
-        prompt = "a headshot of a woman with a dog in space"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-    elif opt.text_type == 15:
-        prompt = "a headshot of a woman with a dog made of marble"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-    elif opt.text_type == 16:
-        prompt = "a headshot of a woman made of marble with a dog"
-        obj_det_cats = ["dog", "person"]
-        test_anchor_locs = [(128.0, 256.0), (384.0, 256.0)]
-        sizes = [180, [200,400]]
-
+    
+    # TODO(Hong-Min): filter the one used in the paper
+    '''
     elif opt.text_type == 17:
         prompt = "a headshot of a woman with a dog"
         obj_det_cats = ["person", "dog"]
@@ -655,6 +392,7 @@ def main():
         obj_det_cats = ["dog", "person"]
         test_anchor_locs = [(150.0, 128.0), (384.0, 256.0)]
         sizes = [[275,200], [200,400]]
+    '''
 
 
 
@@ -689,16 +427,11 @@ def main():
         return objd_cond
  
     def draw_box(img, pred):
-        print(obj_categories)
         labels = [obj_categories[j] for j in pred["labels"].cpu()]
         uint8_image = (img.cpu() * 255).to(torch.uint8)
-        font_size = 10
-        if opt.big_text:
-            font_size = 50
         box = draw_bounding_boxes(uint8_image, boxes=pred["boxes"].cpu(),
                 labels=labels,
-                colors="red", font="/usr/share/fonts/google-crosextra-carlito/Carlito-Regular.ttf",
-                width=4, font_size=font_size)
+                colors="red", width=4)
         box = box.float() / 255.0
         box = box * 2 - 1
         return box
@@ -711,65 +444,31 @@ def main():
     tic = time.time()
     all_samples = list()
 
-    for n in trange(opt.n_iter, desc="Sampling"):
+    for n in trange(opt.trials, desc="Sampling"):
 
         uc = None
         if opt.scale != 1.0:
             uc = model.module.get_learned_conditioning(batch_size * [""])
         c = model.module.get_learned_conditioning([prompt])
-        for multiple_tries in range(1):
-            shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
-            samples_ddim = sampler.sample_seperate(S=opt.ddim_steps,
-                                             conditioning=c,
-                                             batch_size=opt.n_samples,
-                                             shape=shape,
-                                             verbose=False,
-                                             unconditional_guidance_scale=opt.scale,
-                                             unconditional_conditioning=uc,
-                                             eta=opt.ddim_eta,
-                                             operated_image=og_img_guide,
-                                             operation=operation)
 
-            x_samples_ddim = model.module.decode_first_stage(samples_ddim)
-            x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+        shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
+        samples_ddim = sampler.sample_seperate(S=opt.ddim_steps,
+                                         conditioning=c,
+                                         batch_size=opt.n_samples,
+                                         shape=shape,
+                                         verbose=False,
+                                         unconditional_guidance_scale=opt.scale,
+                                         unconditional_conditioning=uc,
+                                         eta=opt.ddim_eta,
+                                         operated_image=og_img_guide,
+                                         operation=operation)
 
-            box_pred = operation.operation_func(x_samples_ddim)
-            
-            def get_black_image(img):
-                img_new = img.clone()
-                img_new[:] = -1.0
-                return img_new
+        x_samples_ddim = model.module.decode_first_stage(samples_ddim)
+        x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
 
-            utils.save_image(x_samples_ddim, f'{results_folder}/new_img_{n}_{multiple_tries}.png')
-            for b in range(opt.n_samples):
-                box_original_output = draw_box(x_samples_ddim[b].detach(), og_img_guide[b])
-                box_pred_output = draw_box(x_samples_ddim[b].detach(), box_pred[b])
-
-                black_background_box = draw_box(get_black_image(x_samples_ddim[b].detach()), og_img_guide[b])
-                img_ = return_cv2(box_original_output, f'{results_folder}/box_new_img_batch_{n}_index_{b}_try_{multiple_tries}.png')
-                img_ = return_cv2(box_pred_output, f'{results_folder}/pred_box_new_img_batch_{n}_index_{b}_try_{multiple_tries}.png')
-                img_ = return_cv2(black_background_box, f'{results_folder}/black_box_new_img_batch_{n}_index_{b}_try_{multiple_tries}.png')
-
-            def save_box(pred, path):
-                pred_cpu = []
-                for item in pred:
-                    item_cpu = {}
-                    for k in item:
-                        item_cpu[k] = item[k].cpu()
-                    pred_cpu.append(item_cpu)
-                with open(path, 'wb') as f:
-                    pickle.dump(pred_cpu, f)
-
-            save_box(og_img_guide, f'{results_folder}/box_original_batch_{n}')
-            try:
-                save_box(box_pred, f'{results_folder}/box_pred_batch_{n}')
-            except:
-                pass
-
-
-
-
-
+        utils.save_image(x_samples_ddim, f'{results_folder}/new_img_{n}.png')
+        box_original_output = draw_box(x_samples_ddim[0].detach(), og_img_guide[0])
+        img_ = return_cv2(box_original_output, f'{results_folder}/box_new_img_{n}.png')
 
 
 if __name__ == "__main__":
